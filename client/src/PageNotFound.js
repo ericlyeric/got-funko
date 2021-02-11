9a664f783dbc2e08b7ea41dec7e85b5863ac5c19
ericlyeric <ericlyeric@gmail.com>
2021-02-10 20:18:44 -0800

    add PageNotFound

--- client/src/PageNotFound.js
+++ client/src/PageNotFound.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import { Container, Button } from 'react-bootstrap';
 
-const PageNotFound = () => <h1>PageNotFound</h1>;
+const PageNotFound = () => (
+  <Container className="text-center pt-5 pb-5">
+    <h1>404 page not found</h1>
+    <p>The page you are trying to visit could not be found</p>
+    <Button>Go to homepage</Button>
+  </Container>
+);
+
 export default PageNotFound;
