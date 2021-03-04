be95a03640b42251b1db9fd766679025af4196b6
ericlyeric <ericlyeric@gmail.com>
2021-03-03 19:48:34 -0800

    tweaks to package and json

--- service/index.js
+++ service/index.js
@@ -18,6 +18,9 @@ if (process.env.NODE_ENV === "development") {
 
 const port = process.env.PORT || 3001;
 connectToDb();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -41,9 +44,8 @@ app.use((req, res, next) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "../client/build.index.html"));
+    res.sendFile(path.join(__dirname, "../client/build.index.html"));
   });
 }
 
