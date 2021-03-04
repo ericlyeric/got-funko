fcca1a0c70e26a7503df762d457a84ea7710a0f8
ericlyeric <ericlyeric@gmail.com>
2021-03-03 19:13:04 -0800

    update package and index of service

--- service/index.js
+++ service/index.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-var cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const { connectToDb } = require('./config/connection');
+const express = require("express");
+const cors = require("cors");
+var cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const { connectToDb } = require("./config/connection");
 
 const app = express();
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   app.use(
     cors({
       origin: process.env.CLIENT_URL,
-    }),
+    })
   );
-  app.use(morgan('dev'));
+  app.use(morgan("dev"));
 }
 
 const port = process.env.PORT || 3001;
@@ -25,8 +25,8 @@ app.use(cors());
 app.use(helmet());
 
 // routes go here
-const authRouter = require('./routes/auth.route');
-const userRouter = require('./routes/user.route');
+const authRouter = require("./routes/auth.route");
+const userRouter = require("./routes/user.route");
 
 app.use(`${process.env.BASE_API_URL}`, authRouter);
 app.use(`${process.env.BASE_API_URL}/user`, userRouter);
@@ -36,16 +36,17 @@ console.log(process.env.PORT);
 app.use((req, res, next) => {
   res.status(404).json({
     success: false,
-    message: 'Page not found',
+    message: "Page not found",
   });
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "../client/build.index.html"));
+  });
 }
 
 app.listen(port, function () {
-  console.log(
-    `Server started, listening at http://localhost:${port}`,
-  );
+  console.log(`Server started, listening at http://localhost:${port}`);
 });
